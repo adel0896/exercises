f03ddf3aefad10d0784e2122932974bc859bc282
Adelina Radulescu <adel0896@stud.kea.dk>
2021-08-29 15:52:32 +0200

    final solution

--- calculator_files/script.js
+++ calculator_files/script.js
@@ -1,58 +1,98 @@
 "use strict";
-// window.addEventListener("DOMContentLoaded", start);
-// function start() {
-//   console.log("function start works");
-// }
-const firstNumber = Number(document.querySelector("#firstnumber").value);
-console.log(firstNumber);
-const secondNumber = Number(document.querySelector("#secondnumber").value);
-console.log(secondNumber);
-const dropvalue = document.querySelector("#operator").value;
-let result;
-// document.querySelector("#operator").addEventListener("click", checkvalue);
-// function checkvalue() {
-//   console.log("function check value works");
-// }
+window.addEventListener("DOMContentLoaded", settingUp);
 
-if (dropvalue === "add") {
-  console.log(dropvalue);
-  document.querySelector("#calculate").addEventListener("click", calculatesum);
-  function calculatesum() {
-    console.log("function calculate sum works");
-    const result = Number(firstNumber + secondNumber);
-    console.log(result);
-    showresult(result);
-    // document.querySelector("#firstnumber").value = result;
-  }
+let firstNumber = Number(document.querySelector("#firstnumber").value);
+console.log(firstNumber);
+let secondNumber = Number(document.querySelector("#secondnumber").value);
+console.log(secondNumber);
+let dropvalue = document.querySelector("#operator").value;
+let dropdecimal = document.querySelector("#decimals").value;
+let checkbox = document.querySelector("#doround").checked;
+console.log(checkbox);
+let result;
+
+function settingUp() {
+  document.querySelector("#firstnumber").addEventListener("input", readNumbers);
+  document.querySelector("#secondnumber").addEventListener("input", readNumbers);
+  console.log("function setting up works");
+  document.querySelector("#operator").addEventListener("change", readOperator);
+  document.querySelector("#calculate").addEventListener("click", calculation);
+  document.querySelector("#decimals").addEventListener("change", readDecimals);
+  document.querySelector("#doround").addEventListener("change", readCheckBox);
+  document.querySelector("#clear").addEventListener("click", clearResult);
 }
-if (dropvalue === "sub") {
-  console.log(dropvalue);
-  document.querySelector("#calculate").addEventListener("click", calculatemin);
-  function calculatemin() {
-    console.log("function calculate min works");
-    const result = Number(firstNumber - secondNumber);
-    console.log(result);
-  }
+function readNumbers() {
+  console.log("readNumbers");
+
+  firstNumber = Number(document.querySelector("#firstnumber").value);
+  secondNumber = Number(document.querySelector("#secondnumber").value);
+
+  console.log(firstNumber);
+  console.log(secondNumber);
 }
-if (dropvalue === "mul") {
+function readOperator() {
+  console.log("readOperator");
+  readNumbers();
+
+  dropvalue = document.querySelector("#operator").value;
   console.log(dropvalue);
-  document.querySelector("#calculate").addEventListener("click", calculatemul);
-  function calculatemul() {
-    console.log("function calculate mul works");
-    const result = Number(firstNumber * secondNumber);
-    console.log(result);
-  }
 }
-if (dropvalue === "div") {
-  console.log(dropvalue);
-  document.querySelector("#calculate").addEventListener("click", calculatediv);
-  function calculatediv() {
-    console.log("function calculate div works");
-    const result = Number(firstNumber / secondNumber);
-    console.log(result);
-  }
+function readCheckBox() {
+  console.log("readCheckbox function works");
+  checkbox = document.querySelector("#doround").checked;
+  console.log(checkbox);
 }
-function showresult(result) {
+
+function readDecimals() {
+  console.log("readDecimals");
+
+  dropdecimal = document.getElementById("decimals").value;
+  console.log(dropdecimal);
+}
+function calculation() {
+  console.log("calculation works");
+
+  if (dropvalue === "add") {
+    console.log(dropvalue);
+
+    result = Number(firstNumber + secondNumber);
+  }
+  if (dropvalue === "sub") {
+    console.log(dropvalue);
+    result = Number(firstNumber - secondNumber);
+  }
+  if (dropvalue === "mul") {
+    console.log(dropvalue);
+    result = Number(firstNumber * secondNumber);
+  }
+  if (dropvalue === "div") {
+    console.log(dropvalue);
+    result = Number(firstNumber / secondNumber);
+  }
+  if (checkbox === true) {
+    readDecimals();
+    result = result.toFixed(dropdecimal);
+  }
+  if (checkbox === false) {
+    result = result;
+  }
+  showresult(result);
+  readNumbers();
+  console.log(result);
+}
+
+function showresult() {
   console.log("show result works");
   document.querySelector("#firstnumber").value = result;
+
+  let li = document.createElement("li");
+  const resultNode = document.createTextNode(result.toString());
+  li.appendChild(resultNode);
+  document.querySelector("#results").appendChild(li);
+  document.querySelector("#results").scrollTo(0, 10000000);
+}
+
+function clearResult() {
+  console.log("clear result");
+  document.querySelector("#results").innerHTML = "";
 }
