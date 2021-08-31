2b6c440b26ec1171b241d7305554e87c52584030
Adelina Radulescu <adel0896@stud.kea.dk>
2021-08-31 11:12:15 +0200

    more exercises

---
+++ capitalization3.js
@@ -0,0 +1,8 @@
+"use strict";
+const name1 = "peter";
+
+const name2 = name1.substring(0, 2).toLowerCase() + name1[2].toUpperCase() + name1.substring(3, 5).toLowerCase();
+console.log(name2);
+const name3 = "Adelina";
+const name4 = name3.substring(0, 2).toLowerCase() + name3[2].toUpperCase() + name3.substring(3).toLowerCase();
+console.log(name4);
---
+++ capitalize.js
@@ -0,0 +1,5 @@
+"use strict";
+const name1 = "adelina stefania radulescu";
+
+const name2 = name1[0].toUpperCase() + name1.substring(1).toLowerCase();
+console.log(name2);
