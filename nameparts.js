8e8e854777347255c77dbefbe3116aab01f77fca
Adelina Radulescu <adel0896@stud.kea.dk>
2021-09-06 14:54:43 +0200

    exercise name

--- nameparts.js
+++ nameparts.js
@@ -1,17 +1,21 @@
-const fullname = "Peter Heronimous Lind";
-const firstname = fullname.substring(fullname.lastIndexOf(), fullname.indexOf(" "));
-const middlename = fullname.substring(fullname.indexOf(" "), fullname.lastIndexOf(" "));
-// middle name also work like const middlename = fullname.substring(fullname.indexOf(" ") + 1, full.name.lastIndexOf(" "));
-console.log(middlename);
-console.log(firstname);
-const lastname = fullname.substring(fullname.lastIndexOf(" ") + 1);
-console.log(lastname);
+// const fullname = "Peter Heronimous Lind";
+// const firstname = fullname.substring(fullname.lastIndexOf(), fullname.indexOf(" "));
+// const middlename = fullname.substring(fullname.indexOf(" "), fullname.lastIndexOf(" "));
+// // middle name also work like const middlename = fullname.substring(fullname.indexOf(" ") + 1, full.name.lastIndexOf(" "));
+// console.log(middlename);
+// console.log(firstname);
+// const lastname = fullname.substring(fullname.lastIndexOf(" ") + 1);
+// console.log(lastname);
 
 const othername = "Albus Percival Wulfric Brian Dumbledore";
+getNameParts(othername);
+function getNameParts(othername) {
+  const firstName = othername.substring(othername.lastIndexOf(), othername.indexOf(" "));
+  const middleName = othername.substring(othername.indexOf(" ") + 1, othername.lastIndexOf(" ") + 1);
+  const lastName = othername.substring(othername.lastIndexOf(" ") + 1);
 
-const firstName = othername.substring(othername.lastIndexOf(), othername.indexOf(" "));
-const middleName = othername.substring(othername.indexOf(" ") + 1, othername.lastIndexOf(" ") + 1);
-const lastName = othername.substring(othername.lastIndexOf(" ") + 1);
-console.log(lastName);
-console.log(middleName);
-console.log(firstName);
+  //   console.log(lastName);
+  //   console.log(middleName);
+  //   console.log(firstName);
+  console.log({ firstName, middleName, lastName });
+}
