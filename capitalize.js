d810e6a0113699e8502973d3652268398a4ad008
Adelina Radulescu <adel0896@stud.kea.dk>
2021-09-07 12:46:50 +0200

    exercises modified with return

--- capitalize.js
+++ capitalize.js
@@ -3,11 +3,13 @@ const name = "adelina stefania radulescu";
 
 // console.log(name2);
 
-capitalize(name);
+// capitalize(name);
 function capitalize(nameParam) {
-  //   let name2 = nameParam[0].toUpperCase() + nameParam.substring(1).toLowerCase();
-  let name2 = nameParam[0].toUpperCase() + nameParam.substring(1, 8).toLowerCase() + nameParam.substring(8).toUpperCase();
-
-  console.log(name2);
-  console.log(name);
+  let name2 = nameParam[0].toUpperCase() + nameParam.substring(1).toLowerCase();
+  // let name2 = nameParam[0].toUpperCase() + nameParam.substring(1, 8).toLowerCase() + nameParam.substring(8).toUpperCase();
+  return name2;
+  // console.log(name2);
+  // console.log(name);
 }
+const cap = capitalize(name);
+console.log(cap);
--- nameparts.js
+++ nameparts.js
@@ -7,15 +7,42 @@
 // const lastname = fullname.substring(fullname.lastIndexOf(" ") + 1);
 // console.log(lastname);
 
-const othername = "Albus Percival Wulfric Brian Dumbledore";
-getNameParts(othername);
-function getNameParts(othername) {
-  const firstName = othername.substring(othername.lastIndexOf(), othername.indexOf(" "));
-  const middleName = othername.substring(othername.indexOf(" ") + 1, othername.lastIndexOf(" ") + 1);
-  const lastName = othername.substring(othername.lastIndexOf(" ") + 1);
+// const othername = "Albus Percival Wulfric Brian Dumbledore";
+// getNameParts(othername);
+// function getNameParts(othername) {
+//   const firstName = othername.substring(othername.lastIndexOf(), othername.indexOf(" "));
+//   const middleName = othername.substring(othername.indexOf(" ") + 1, othername.lastIndexOf(" ") + 1);
+//   const lastName = othername.substring(othername.lastIndexOf(" ") + 1);
 
-  //   console.log(lastName);
-  //   console.log(middleName);
-  //   console.log(firstName);
-  console.log({ firstName, middleName, lastName });
+//   //   console.log(lastName);
+//   //   console.log(middleName);
+//   //   console.log(firstName);
+//   console.log({ firstName, middleName, lastName });
+// }
+//learning Parameters
+fullName("Potter", "Harry", "James", "Pottypotpot");
+function fullName(lastName, firstName, middleName) {
+  if (middleName == undefined) {
+    fullname = `${firstName} ${lastName}`;
+    console.log(fullname);
+  } else {
+    fullname = `${firstName} ${middleName} ${lastName}  `;
+    console.log(fullname);
+  }
 }
+//learning return
+let firstname;
+function greetings(firstname) {
+  return `Hello ${firstname}`;
+}
+result = greetings("peter");
+console.log(greetings("any name"));
+console.log(result);
+greetings();
+const txt = `Greeting is ${greetings("a name")}`;
+console.log(txt);
+Math.random();
+console.log(Math.random());
+console.log(greetings("is it me your are looking for?"));
+console.log(greetings("is it me your are looking for?").indexOf("me"));
+console.log(Math.floor(Math.random) * greetings("is it me your are looking for?").indexOf("me"));
